c39c36e77615eee3130528bc75735dd6b49312a5
Martix <martin.quivron@icloud.com>
2022-01-09 19:03:39 +0100

    fix/permissionsCommands

--- events/client/ready.js
+++ events/client/ready.js
@@ -14,47 +14,6 @@ module.exports = async (Client) => {
 
     deploySlashCommands(Client)
 
-    if (!Client.application.owner) await Client.application.fetch();
-
-Client.guilds.cache.get(config.guildID).commands.fetch().then(collection =>{
-    collection.forEach(command =>{
-        if(command.applicationId === config.id && command.guildId == config.guildID){
-            if(! /ban+tempban+kick+lock+unlock+warn+sanctions+clear+mute+unmuet+partial-ban/.test(command.name))return;
-
-            let guild = Client.guilds.cache.get(config.guildID)
-            guild.commands.permissions.set({command: command.id, permissions: [
-                {
-                    id: config.AdoID,
-                    type: 'ROLE',
-                    permission: false,
-                },
-                {
-                    id: Client.guilds.cache.get(config.guildID).roles.everyone.id,
-                    type: 'ROLE',
-                    permission: false,
-                },
-                {
-                    id: config.AdulteID,
-                    type: 'ROLE',
-                    permission: false,
-                },
-                {
-                    id: config.NonVerifID,
-                    type: 'ROLE',
-                    permission: false,
-                },
-                {
-                    id: config.StaffID,
-                    type: 'ROLE',
-                    permission: true,
-                },
-            ]}).catch(console.log);
-
-        }
-    })
-
-    
-})
     console.log('BotLog : Commandes slash correctement enregistrées !')
 
 }
--- startup.js
+++ startup.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const Discord = require('discord.js')
 const { token, id } = require('./util/config');
-
+const config = require('./util/config')
 const loadEvents = (Client) => {
     fs.readdirSync(`./events`).forEach(dirs => {
         const events = fs.readdirSync(`./events/${dirs}/`).filter(files => files.endsWith(".js"));
@@ -45,10 +45,50 @@ client.guilds.cache.forEach((g)=>{
         return 
     });
 }
-
+fixPermissions(client)
 }
 
-
+const fixPermissions = (client)=>{
+    client.guilds.cache.get(config.guildID).commands.fetch().then(collection =>{
+        collection.forEach(command =>{
+            if(command.applicationId === config.id && command.guildId == config.guildID){
+                if(! /ban+tempban+kick+lock+unlock+warn+sanctions+clear+mute+unmuet+partial-ban/.test(command.name))return;
+    
+                let guild = client.guilds.cache.get(config.guildID)
+                guild.commands.permissions.set({command: command.id, permissions: [
+                    {
+                        id: config.AdoID,
+                        type: 'ROLE',
+                        permission: false,
+                    },
+                    {
+                        id: client.guilds.cache.get(config.guildID).roles.everyone.id,
+                        type: 'ROLE',
+                        permission: false,
+                    },
+                    {
+                        id: config.AdulteID,
+                        type: 'ROLE',
+                        permission: false,
+                    },
+                    {
+                        id: config.NonVerifID,
+                        type: 'ROLE',
+                        permission: false,
+                    },
+                    {
+                        id: config.StaffID,
+                        type: 'ROLE',
+                        permission: true,
+                    },
+                ]}).catch(console.log);
+    
+            }
+        })
+    
+        
+    })
+}
 module.exports = {
     loadEvents,
     deploySlashCommands
